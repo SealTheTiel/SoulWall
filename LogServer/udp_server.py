85940fe89bf42e98ec3c16b7ba1925857b6abd40
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2025-03-05 20:00:45 +0800

    added support for modal logging

--- LogServer/udp_server.py
+++ LogServer/udp_server.py
@@ -2,22 +2,24 @@ import socket
 import threading
 import csv
 import time
+from pathlib import Path
 
 BROADCAST_PORT = 9001
 DATA_PORT = 9000
 
-CSV_FILE = f"Data/{time.strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+DATA_DIR = f"Data/{time.strftime('%Y-%m-%d_%H-%M-%S')}"
+Path(DATA_DIR).mkdir(parents=True, exist_ok=True)
 
-def get_local_ip():
+def getLocalIp():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     ip = s.getsockname()[0]
     s.close()
     return ip
 
-SERVER_IP = get_local_ip()
+SERVER_IP = getLocalIp()
 
-def broadcast_ip():
+def broadcastIp():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     
@@ -26,24 +28,44 @@ def broadcast_ip():
         sock.sendto(message, ("<broadcast>", BROADCAST_PORT))
         time.sleep(5)
 
-def receive_data():
+def writeData(data):
+    filename = data[0]
+    data = data[1:]
+    file = open(f"{DATA_DIR}/{filename}.csv", mode='a', newline='')
+    writer = csv.writer(file)
+    writer.writerow(data)
+    file.close()
+
+def setupFiles():
+    file = open(f"{DATA_DIR}/modal_transform.csv", mode='w', newline='')
+    writer = csv.writer(file);
+    writer.writerow(["Timestamp", "Modal ID", "Position X", "Position Y", "Position Z", "Rotation X", "Rotation Y", "Rotation Z", "Scale X", "Scale Y", "Scale Z"])
+    file.close()
+
+    file = open(f"{DATA_DIR}/modal_actions.csv", mode='w', newline='')
+    writer = csv.writer(file);
+    writer.writerow(["Timestamp", "Modal ID", "Action"])
+    file.close()
+
+    file = open(f"{DATA_DIR}/eagle_focus.csv", mode='w', newline='')
+    writer = csv.writer(file);
+    writer.writerow(["Timestamp", "Eagle ID", "Focus"])
+    file.close()
+
+
+
+def receiveData():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((SERVER_IP, DATA_PORT))
 
-    with open(CSV_FILE, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Timestamp", "X", "Y", "Z"])  # Write header
+    while True:
+        data, addr = sock.recvfrom(1024)
+        data = data.decode().split(",")
+        writeData(data)
 
-        while True:
-            data, addr = sock.recvfrom(1024)
-            decoded_data = data.decode()
-            print(f"Received: {decoded_data} from {addr}")
-            writer.writerow(decoded_data.split(","))
+setupFiles()
+threading.Thread(target=broadcastIp, daemon=True).start()
+threading.Thread(target=receiveData, daemon=True).start()
 
-# Start server threads
-threading.Thread(target=broadcast_ip, daemon=True).start()
-threading.Thread(target=receive_data, daemon=True).start()
-
-# Keep script running
 while True:
     time.sleep(1)
